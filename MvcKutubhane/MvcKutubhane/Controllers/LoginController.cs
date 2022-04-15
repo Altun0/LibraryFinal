a8e75e3845882958194fcd68a62d40a1eab12d81
Altun0 <altunna@code.edu.az>
2022-04-15 14:22:06 -0800

    commit240

--- MvcKutubhane/MvcKutubhane/Controllers/LoginController.cs
+++ MvcKutubhane/MvcKutubhane/Controllers/LoginController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcKutubhane.Models.Entity;
+using System.Web.Security;
 namespace MvcKutubhane.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        DBKUTUBHANEEntities db = new DBKUTUBHANEEntities();
         public ActionResult GirisYap()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GirisYap(TBLUYELER p)
+        {
+            var bilgiler = db.TBLUYELERs.FirstOrDefault(x => x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
     }
 }
---
+++ MvcKutubhane/MvcKutubhane/Controllers/PnlController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutubhane.Controllers
+{
+    public class PnlController : Controller
+    {
+        // GET: Pnl
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
